ddd9357cc1bb1611b58b4ac310bd884e94ab19f7
Ian Carlos <carlos.ian007@gmail.com>
2019-05-07 15:40:47 -0300

    Feat(debug): Add Reactotron Config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import './config/ReactotronConfig';
 
 import TodoList from './TodoList';
 import store from './store';
+
+console.tron.log('testendo');
 
 const App = () => (
   <Provider store={store}>
